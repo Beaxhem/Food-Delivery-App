2d110a4854882b8182f55a114a2e3421f5cdcc9d
Ilya <senchukov02@gmail.com>
2020-09-25 10:09:59 +0300

    Initial Commit

---
+++ Food Delivery/Food_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DeliveryApp.swift
+//  Food Delivery
+//
+//  Created by Ilya Senchukov on 25.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Food_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
